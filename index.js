8d62698bc7ff5ab419c79e474a33971ceab20abb
Tyrin Holloway <tychnk@gmail.com>
2021-10-21 13:44:53 -0500

    added route for adding new animals

--- index.js
+++ index.js
@@ -16,10 +16,22 @@ mongoose.connect('mongodb://localhost:27017/animalAdoption')
 
     app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'ejs');
+    app.use(express.urlencoded({extended : true}))
 
     app.get('/animals', async (req, res) => {
         const animals = await Animal.find({})
         res.render('animals/index', { animals })
+
+    })
+
+    app.get('/animals/new', (req, res) => {
+        res.render('animals/new')
+    })
+
+    app.post('/animals', async (req, res) => {
+        const newAnimal = new Animal(req.body)
+        await newAnimal.save();
+        res.redirect(`/animals/${newAnimal._id}`)
 
     })
     app.get('/animals/:id', async (req, res) => {
